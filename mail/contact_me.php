e9678c6b1f6a0879b8143603b5200ee1543a5188
divyansh <divyanshojha2003@gmail.com>
2022-02-05 13:38:00 +0530

    some changes

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,7 +14,7 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'info@divyanshojha.ml';
+$to = 'divyanshojha2003@gmail.com';
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
 $headers = "From: $email_address";
