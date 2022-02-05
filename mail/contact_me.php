b1996ff5754e82e1eea6d707edb52df60dda68f2
divyansh <divyanshojha2003@gmail.com>
2022-02-05 13:30:07 +0530

    added

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,7 +17,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'info@divyanshojha.ml';
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@divyanshojha.ml";
+$headers = "From: $email_address";
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
